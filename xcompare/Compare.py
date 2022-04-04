5329e7e9d3361cb1b73fc01adf9e91fc648abfe2
John Krasting <John.Krasting@noaa.gov>
2022-04-04 19:16:03 -0400

    General fixes to Compare class and compare_xy.py

--- xcompare/Compare.py
+++ xcompare/Compare.py
@@ -3,6 +3,7 @@
 import warnings
 import xarray as xr
 from xcompare import compare_xy
+from xcompare import coord_util
 
 __all__ = ["Compare"]
 
@@ -16,34 +17,54 @@ class Compare:
         weights=None,
         save_weights=True,
         resolution=None,
+        coord=None,
     ):
 
         validate_objects(obj1, obj2)
-        self.comparables = (obj1, obj2)
 
-        if isinstance(obj1, xr.DataArray):
-            if verbose:
-                print("Performing Comparison on Arrays")
-            result = compare_xy.compare_arrays(
-                *self.comparables,
-                verbose=verbose,
-                weights=weights,
-                save_weights=save_weights,
-                resolution=resolution,
-            )
+        obj1 = obj1.squeeze()
+        obj2 = obj2.squeeze()
 
-        elif isinstance(obj1, xr.Dataset):
-            if verbose:
-                print("Performing Comparison on Datasets")
-            result = compare_xy.compare_datasets(
-                *self.comparables,
-                verbose=verbose,
-                weights=weights,
-                save_weights=save_weights,
-                resolution=resolution,
-            )
+        # check coordinates before proceeding
+        if coord is None:
+            coord = coord_util.infer_coordinate_system(obj1)
+            coords2 = coord_util.infer_coordinate_system(obj2)
+            assert coord == coords2
 
-        (self.difference, self.weights) = result
+        if coord == "yx":
+
+            # catch and fix erroneous bounds
+            obj1 = coord_util.fix_bounds_attributes(obj1)
+            obj2 = coord_util.fix_bounds_attributes(obj2)
+
+            self.comparables = (obj1, obj2)
+
+            if isinstance(obj1, xr.DataArray):
+                if verbose:
+                    print("Performing Comparison on Arrays")
+                result = compare_xy.compare_arrays(
+                    *self.comparables,
+                    verbose=verbose,
+                    weights=weights,
+                    save_weights=save_weights,
+                    resolution=resolution,
+                )
+
+            elif isinstance(obj1, xr.Dataset):
+                if verbose:
+                    print("Performing Comparison on Datasets")
+                result = compare_xy.wrap_compare_datasets(
+                    *self.comparables,
+                    verbose=verbose,
+                    weights=weights,
+                    resolution=resolution,
+                )
+
+            (self.difference, self.weights) = result
+
+        else:
+
+            raise ValueError(f"Coordinate system f{coord} is not currently supported")
 
 
 def validate_objects(obj1, obj2):
--- xcompare/compare_xy.py
+++ xcompare/compare_xy.py
@@ -4,12 +4,25 @@ import warnings
 import xesmf as xe
 import xarray as xr
 
-from xcompare.coord_util import rename_coords_xy, identical_xy_coords
+from xcompare.coord_util import (
+    rename_coords_xy,
+    identical_xy_coords,
+    list_dset_dimset,
+    fix_bounds_attributes,
+    valid_xy_dims,
+    extract_dimset,
+)
 from xcompare.standard_grid import generate_standard_grid
 from xcompare.xr_stats import xr_stats_2d
 
 
-__all__ = ["compare_arrays", "determine_target", "compare_datasets"]
+__all__ = [
+    "compare_arrays",
+    "determine_target",
+    "compare_datasets",
+    "regrid_dataset",
+    "wrap_compare_datasets",
+]
 
 
 def compare_arrays(
@@ -106,56 +119,20 @@ def compare_datasets(
     # loop over the datasets and regrid if they do not match the target grid
     comparables = list(comparables)
 
+    weights_ds = None
+
     for num, comp in enumerate(comparables):
         if not identical_xy_coords(comp, target):
 
-            if verbose:
-                print("Starting regridder")
-                if weights is not None:
-                    print(f"Using precalculated weights file: {weights}")
-                else:
-                    print("Generating weights - this step may a take a while.")
-
-            try:
-                regridder = xe.Regridder(
-                    comp,
-                    target,
-                    "bilinear",
-                    weights=weights,
-                    unmapped_to_nan=True,
-                )
-            except Exception as exception:
-                if weights is not None:
-                    warnings.warn(str(exception))
-                    exception = RuntimeError(
-                        "Regridding failed. "
-                        + "Check weights file or remove to regenerate weights."
-                    )
-                raise exception
-
-            if save_weights:
-                wgt = regridder.weights.data
-                dim = "n_s"
-                weights_ds = xr.Dataset(
-                    {
-                        "S": (dim, wgt.data),
-                        "col": (dim, wgt.coords[1, :] + 1),
-                        "row": (dim, wgt.coords[0, :] + 1),
-                    }
-                )
-
-            else:
-                weights_ds = None
-
-            # perform the regridding
-            comparables[num] = regridder(comp)
+            comparables[num], weights_ds = regrid_dataset(
+                comp, target, weights=weights, verbose=verbose
+            )
 
     # difference the two datasets
     diff = comparables[0] - comparables[1]
 
     # calculate comparison statistics
     if stats:
-
         try:
             area = target.cf["area"]
         except KeyError as _:
@@ -175,3 +152,113 @@ def compare_datasets(
     result = (diff, weights_ds)
 
     return result
+
+
+def regrid_dataset(source, target, weights=None, verbose=False):
+    if verbose:
+        print("Starting regridder")
+        if weights is not None:
+            print(f"Using precalculated weights file: {weights}")
+        else:
+            print("Generating weights - this step may a take a while.")
+
+    try:
+        regridder = xe.Regridder(
+            source,
+            target,
+            "bilinear",
+            weights=weights,
+            unmapped_to_nan=True,
+        )
+    except Exception as exception:
+        if weights is not None:
+            warnings.warn(str(exception))
+            exception = RuntimeError(
+                "Regridding failed. "
+                + "Check weights file or remove to regenerate weights."
+            )
+        raise exception
+
+    wgt = regridder.weights.data
+    dim = "n_s"
+    weights_ds = xr.Dataset(
+        {
+            "S": (dim, wgt.data),
+            "col": (dim, wgt.coords[1, :] + 1),
+            "row": (dim, wgt.coords[0, :] + 1),
+        }
+    )
+
+    # perform the regridding
+
+    return (regridder(source), weights_ds)
+
+
+def wrap_compare_datasets(ds1, ds2, weights=None, verbose=False, resolution=None):
+
+    ds1 = ds1.squeeze()
+
+    if "static_fields" in ds1.attrs.keys():
+        ds1_static_vars = ds1.attrs["static_fields"]
+        ds1_vars = [x for x in list(ds1.keys()) if x not in ds1_static_vars]
+    else:
+        ds1_static_vars = []
+        ds1_vars = ds1.keys()
+
+    dimlist_1 = list_dset_dimset(ds1)
+    ds1_vars = [x for x in ds1_vars if ds1[x].dims in dimlist_1]
+
+    if "static_fields" in ds2.attrs.keys():
+        ds2_static_vars = ds2.attrs["static_fields"]
+        ds2_vars = [x for x in list(ds2.keys()) if x not in ds2_static_vars]
+    else:
+        ds2_static_vars = []
+        ds2_vars = ds2.keys()
+
+    dimlist_2 = list_dset_dimset(ds2)
+    ds2_vars = [x for x in ds2_vars if ds2[x].dims in dimlist_2]
+
+    varlist = list(set(ds1_vars).intersection(set(ds2_vars)))
+
+    if len(dimlist_1) == 1 and len(dimlist_2) == 1:
+        diff, weights_ds = compare_datasets(
+            ds1, ds2, save_weights=True, verbose=verbose, resolution=resolution
+        )
+
+    else:
+        warnings.warn("Datasets have multiple coordinates")
+        target_ds = generate_standard_grid()
+
+        _ds1 = []
+
+        for dim in dimlist_1:
+            if valid_xy_dims(ds1, dim):
+                result = regrid_dataset(extract_dimset(ds1, dim), target_ds)
+                _ds1.append(result[0])
+
+        _ds1 = xr.merge(_ds1)
+
+        for var in ds1_static_vars:
+            if var in list(_ds1.keys()):
+                _ds1 = _ds1.drop(var)
+
+        _ds2 = []
+        for dim in dimlist_2:
+            if valid_xy_dims(ds2, dim):
+                result = regrid_dataset(extract_dimset(ds2, dim), target_ds)
+                _ds2.append(result[0])
+
+        _ds2 = xr.merge(_ds2)
+
+        for var in ds2_static_vars:
+            if var in list(_ds2.keys()):
+                _ds2 = _ds2.drop(var)
+
+        _ds2 = _ds2.merge(target_ds)
+
+        diff, weights_ds = compare_datasets(_ds1, _ds2, verbose=verbose)
+
+    for var in list(diff.keys()):
+        diff[var].attrs = {**ds1[var].attrs, **diff[var].attrs}
+
+    return diff, weights_ds
